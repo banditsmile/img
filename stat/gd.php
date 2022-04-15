ebb12a8b5afdef0357ae5f65303e6347507f6dea
xulianhong <lianhong.xu@cnlaunch.com>
2022-04-15 18:23:41 +0800

    通过3d图像展示像素颜色分布辅助去水印研究

--- stat/gd.php
+++ stat/gd.php
@@ -5,16 +5,20 @@ $im = ImageCreateFromjpeg($imageSrc);
 list($width, $height, $type, $attr) = getimagesize($imageSrc);
 
 $color = [];
-for($i=0;$i<$width;$i++){
-    for($j=0;$j<$height;$j++){
+$length = $_GET['length'];
+$count = 0;
+for($i=$width/2;$i<$width;$i++){
+    for($j=$height/2;$j<$height;$j++){
         $rgb = ImageColorAt($im, 100, 100);
         $r = ($rgb >> 16) & 0xFF;
         $g = ($rgb >> 8) & 0xFF;
         $b = $rgb & 0xFF;
         $color[]=[$r,$g,$b];
+        if($count++>$length){
+            break 2;
+        }
     }
 }
-$color = array_slice($color,0,100);
 header("Content-type:application/json");
 echo json_encode($color,0,1000);
 exit();
